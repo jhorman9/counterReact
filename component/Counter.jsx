dd2b9476aa4aca98cf6330c4f4d6b60f4d784868
jhorman9 <jhosmita9@gmail.com>
2022-10-22 14:00:31 -0500

    counter with react

---
+++ component/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+const Counter = () =>{
+    const [counter,setCounter] = useState(0);
+    let counterOn = () =>{
+        setCounter(counter+1)
+    }
+    return (
+        <>
+            <div className="counter">{counter}</div>
+            <button className="counterBtn" onClick={counterOn}>Counter</button>
+        </>
+    )
+}
+
+export default Counter;
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Counter from '../component/Counter'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <div className="content">
+          <Counter/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
